6c0836b2e8f503edf1a323002e1afc42c622a6c4
DevGaikwad <141038667+DevGaikwad@users.noreply.github.com>
2023-12-05 09:46:06 +0530

    Update operaters.c

--- operaters.c
+++ operaters.c
@@ -10,5 +10,34 @@ int main() {
     printf("%d\n", mult);
     printf("%d\n", div);
     printf("%d\n",module );
+
+    int a=20;
+    int b=30;
+
+    printf("%d\n", a==b);
+          printf("%d\n", a !=b);
+          printf("%d\n", a >b);
+          printf("%d\n", a <b);
+          printf("%d\n", a >=b);
+          printf("%d\n", a <=b);
+
+    //Assignment operators
+
+    a += 10;
+  //  a -=15;
+  //  a*=10;
+  //  a/=10;
+  //  a %=10;
+  //  a |=10;
+
+   printf("%d\n", a);
+ //Logical operator
+
+    int x=50;
+    int y=40;
+    printf("%d\n", x==50 && y < 60 );  
+      printf("%d\n", x ==50 || y > x );   
+      printf("%d", !(x==50 || y > 60) );
+    
     return 0;
 }
